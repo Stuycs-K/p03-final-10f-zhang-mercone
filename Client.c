8425341f22507dd5d29170cfd143d4d9f48017a4
Lucia Mercone <lmercone60@stuy.edu>
2026-01-07 07:51:58 -0500

    small commenting + housekeeping, naptime

--- Client.c
+++ Client.c
@@ -1,30 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+#include <netdb.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include 'Networking.h'
-// The purpose of this .c is to send stuff (like user input) to server 
-// or to get stuff (like results) from server. 
+#include 'GameLogic.h'
+// The purpose of this .c is to send stuff (like user input) to server
+// or to get stuff (like results) from server.
 
 
-	// Asks for user move and sends to server. Then waits for server response for the game. 
-	// Displays the result to user. 
-	// Later: can have the option to ask for username, send to server, and get opponent's username and display etc. 
-	// Later 2: after each match ask if the users want another game. Redo if both replied yes. 
-	// Later 3: if no response after a certain time duration (e.g. 10s) quit automatically. 
-	
-	void client_logic(int server_socket){ 
-		
+	// Asks for user move and sends to server. Then waits for server response for the game.
+	// Displays the result to user.
+	// Later: can have the option to ask for username, send to server, and get opponent's username and display etc.
+	// Later 2: after each match ask if the users want another game. Redo if both replied yes.
+	// Later 3: if no response after a certain time duration (e.g. 10s) quit automatically.
+
+	void client_logic(int server_socket){
+
 	}
-	
-	// a wrapper function that calls clinet_connect(char* IP, char* port)
-	// which returns connected socket descriptor. 
-	
+
+	// a wrapper function that calls client_connect(char* IP, char* port)
+	// which returns connected socket descriptor.
+
 	int connect_to_server(char* IP){
-		
+
 	}
-	
-	// optionally takes IP address from user input 
+
+	// optionally takes IP address from user input
 	// connects to the server (call connect_to_server)
-	// runs the client loop (call clinet_loop())
+	// runs the client loop (call client_loop())
 	// close up socket at the end
 	
 	int main(int argc, char* argv[]){
-		
+		char * IP;
+			if (argc > 1){
+				strcpy(IP, argv[1]);
+			}
+
+		connect_to_server(IP);
+
 	}
--- Server.c
+++ Server.c
@@ -12,31 +12,43 @@
 #include 'Networking.h'
 #include 'GameLogic.h'
 
-	// accept clients using server_accept
-	// add clients to waiting queue
-	// when 2 clients available: call fork_subserver(clientA, clinetB)
-	// don't end unless ctrl-c
-	void server_logic_loop(int server_fd){
-		while (1){
+// accept clients using server_accept
+// add clients to waiting queue
+// when 2 clients available: call fork_subserver(clientA, clinetB)
+// don't end unless ctrl-c
+void server_logic_loop(int server_fd, int num_connected){
+	//if a client_server handshake is successful, up num_connected by 1
+	//if num_connected would be >= max #, backlog the connect and refuse it
+}
 
-		}
+// creates a child process to run the match between clientA and clientB
+// Child: closes listening socket, call run_match(clientA_fd, clientB_fd). exit when match ends
+// Parent: closes both client sockets. Return to logic loop.
+void fork_subserver(int clientA_fd, int clientB_fd){
+
+}
+
+//Main server logic
+//Runs server_logic_loop
+int main (){
+	int listen_socket = server_setup();
+	int connected = 0;
+	//one time setup for a listening socket
+	while (1){
+		//server_logic_loop
 	}
+	return 0;
+}
 
-	// creates a child process to run the match between clientA and clientB
-	// Child: closes listening socket, call run_match(clientA_fd, clientB_fd). exit when match ends
-	// Parent: closes both client sockets. Return to logic loop.
 
-	void fork_subserver(int clientA_fd, int clientB_fd){
 
-	}
+// clearer substitute for calling signal(SIGNUM, sighandler)
+// checks for SIGCHLD and SIGINT
+void setup_sighandlers(){
 
-	// clearer substitute for calling signal(SIGNUM, sighandler)
-	// checks for SIGCHLD and SIGINT
-	void setup_sighandlers(){
+}
 
-	}
+// handles SIGCHLD and SIGINT
+static void sighandler(int signo){
 
-	// handles SIGCHLD and SIGINT
-	static void sighandler(int signo){
-
-	}
+}
