b3dec22bf74e67d93bfc0c9e23502159da6712cb
Lucia Mercone <lmercone60@stuy.edu>
2026-01-07 15:22:20 -0500

    Lucia: didn't....save????

---
+++ Networking.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+#include <netdb.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+//server setup
+
+//server_accept
