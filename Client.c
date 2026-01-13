1198a503968b6e56505e46440574df2c9d249f36
Lucia Mercone <lmercone60@stuy.edu>
2026-01-13 16:05:23 -0500

    PING

--- Client.c
+++ Client.c
@@ -26,54 +26,57 @@ void client_logic(int server_socket){
 	int gamesPlayed = 0;
 	printf("%d\n", server_socket);
 	while (gamesPlayed < 3){ //i.e., repeat until 3 rounds have been played
-		printf("Debugging02\n");
-		int bytes_received = recv(server_socket, &gamesPlayed, sizeof(int), 0);
-		if (bytes_received <= 0){
-			perror("Client exiting due to empty message...\n");
-			exit(0);
-		}
-		int moveint = -1;
-
-		while (moveint < 0 || moveint > 2){ //while loop to ensure the sent value is greater than -1
-			printf ("Please enter the number of one of the options:\n\t0. Rock\n\t1. Paper\n\t2. Scissors\n");
-			char move[16];
-			moveint = -1;
-			if(fgets (move, 16, stdin) == NULL){
-				perror("Input closed. ");
+		if (ClientServerPing == 0){
+			int bytes_received = recv(server_socket, &gamesPlayed, sizeof(int), 0);
+			if (bytes_received <= 0){
+				perror("Client exiting due to empty message...\n");
+				exit(0);
 			}
-			if(sscanf (move, "%d", &moveint) != 1){
+			int moveint = -1;
+
+			while (moveint < 0 || moveint > 2){ //while loop to ensure the sent value is greater than -1
+				printf ("Please enter the number of one of the options:\n\t0. Rock\n\t1. Paper\n\t2. Scissors\n");
+				char move[16];
 				moveint = -1;
+				if(fgets (move, 16, stdin) == NULL){
+					perror("Input closed. ");
+				}
+				if(sscanf (move, "%d", &moveint) != 1){
+					moveint = -1;
+				}
+				printf("moveint %d\n", moveint);
 			}
-			printf("moveint %d\n", moveint);
+			send(server_socket, &moveint, sizeof(int), 0);
 		}
-		send(server_socket, &moveint, sizeof(int), 0);
+		else {
+		exit(0);	
+		}
 	}
-		printf("Game ended.\n");
-		exit(0);
+	printf("Game ended.\n");
+	exit(0);
 }
-	// a wrapper function that calls client_connect(char* IP, char* port)
-	// which returns connected socket descriptor.
-	int connect_to_server(char* IP){
-		int sockfd = client_connect(IP, PORT);
-		if (sockfd < 0){
-			perror("client_connect failed");
-		}
-		return sockfd;
+// a wrapper function that calls client_connect(char* IP, char* port)
+// which returns connected socket descriptor.
+int connect_to_server(char* IP){
+	int sockfd = client_connect(IP, PORT);
+	if (sockfd < 0){
+		perror("client_connect failed");
 	}
+	return sockfd;
+}
 
-	// optionally takes IP address from user input
-	// connects to the server (call connect_to_server)
-	// runs the client loop (call client_logic())
-	// close up socket at the end
-	int main(int argc, char* argv[]){
-		char* IP = "127.0.0.1";
-		if (argc > 1){
-			strcpy(IP, argv[1]);
-		}
-
-		int server_socket = connect_to_server(IP);
-		client_logic(server_socket);
-
-		//before exiting, send a 'down' to server to reduce # of connections by 1
-		return (0);
+// optionally takes IP address from user input
+// connects to the server (call connect_to_server)
+// runs the client loop (call client_logic())
+// close up socket at the end
+int main(int argc, char* argv[]){
+	char* IP = "127.0.0.1";
+	if (argc > 1){
+		strcpy(IP, argv[1]);
 	}
+	int server_socket = connect_to_server(IP);
+	client_logic(server_socket);
+
+	//before exiting, send a 'down' to server to reduce # of connections by 1
+	return (0);
+}
